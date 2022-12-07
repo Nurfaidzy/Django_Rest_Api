b8ded932cd6a3b5619193ae0dd9942c6fbe6b0b8
nurfaidzy.riyamizard <nurfaidzy.riyamizard@akulaku.com>
2022-12-07 16:48:12 +0700

    Postgresql

--- DjangoAPI/settings.py
+++ DjangoAPI/settings.py
@@ -78,10 +78,22 @@ WSGI_APPLICATION = 'DjangoAPI.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# sqlite
+# DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'belajar',
+        'USER': 'postgres',
+        'PASSWORD': 'yourpasss',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
